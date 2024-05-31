4eb895204f960748a948272eb1be82061f61a3b1
namka <namarajkarki007@gmail.com>
2024-05-31 11:32:02 +0545

    Displaying result with quote

--- src/components/Results.vue
+++ src/components/Results.vue
@@ -1,14 +1,33 @@
 <template>
   <p>Reaction time: {{ score }}ms</p>
+  <p class="rank">{{ rank }}</p>
 
 </template>
 
 <script>
 export default {
-    props:['score']
+    props:['score'],
+    data(){
+        return {
+           rank:null,
+        }
+    },
+    mounted() {
+        if(this.score < 250){
+            this.rank = "leopard reflex"
+        }else if(this.score < 400){
+            this.rank = "rabbit reflex"
+        }else{
+            this.rank = "snail reflex"
+        }
+    }
 }
 </script>
 
 <style>
-
+    .rank {
+        font-size: 1.4em;
+        color: #0faf87;
+        font-weight: bold;
+    }
 </style>
